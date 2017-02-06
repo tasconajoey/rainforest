b11d250e24a8348a61c67d2bb3e7378fe74c7f49
tasconajoey <joey.tascona@gmail.com>
2017-02-06 14:36:13 -0500

    Writing the review routes, controllers and views

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,6 @@ class Product < ApplicationRecord
     price_in_dollars = price_in_cents.to_f / 100
     sprintf("%.2f", price_in_dollars)
   end
+  has_many :reviews
+  has_many :user, through: :reviews
 end
